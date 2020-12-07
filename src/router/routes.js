3666fa6543512b5113e6b250c75ea5767194a928
Mirfozil-dev <makhmudov03@icloud.com>
2020-12-07 11:43:07 +0500

    router fixed

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,19 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Todo.vue') },
+      { path: '/', component: () => import('pages/Todo.vue') },
       { path: '/help', component: () => import('pages/Help.vue') },
-    ]
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '*',
-    component: () => import('pages/Error404.vue')
-  }
-]
+    component: () => import('pages/Error404.vue'),
+  },
+];
 
-export default routes
+export default routes;
